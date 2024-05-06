d7574f86144af122b07c318ec44e237f4fea594f
Rizki5846 <rizkiardiansyah584@gmail.com>
2024-05-06 12:43:04 +0700

    add migration table

---
+++ database/migrations/2024_05_06_053340_create_dokter_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('dokter', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_doktor', 255);
+            $table->bigInteger('specialist_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dokter');
+    }
+};
